24925325fa1ff41744dfa87176c14590ceb81fff
team-cores <sanket.deshpande@coreviwsystems.com>
2024-01-18 17:11:53 +0530

    added fixes

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+a
+1backend/src/main/resources/application.properties,3/f/3f2f17daa13c76ba02d9d8cf5498cdfd7ebe2ce8
--- backend/src/main/java/com/exp/FinWise/usersOnBoarding/controller/AuthController.java
+++ backend/src/main/java/com/exp/FinWise/usersOnBoarding/controller/AuthController.java
@@ -18,10 +18,7 @@ import com.exp.FinWise.usersOnBoarding.repository.RoleRepository;
 import com.exp.FinWise.usersOnBoarding.repository.UserRepository;
 import com.exp.FinWise.usersOnBoarding.service.UserDetailsImpl;
 import io.micrometer.common.util.StringUtils;
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -39,141 +36,108 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-  @Autowired
-  AuthenticationManager authenticationManager;
+    @Autowired
+    AuthenticationManager authenticationManager;
 
-  @Autowired
-  UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-  @Autowired
-  RoleRepository roleRepository;
+    @Autowired
+    RoleRepository roleRepository;
 
-  @Autowired
-  PasswordEncoder encoder;
+    @Autowired
+    PasswordEncoder encoder;
 
-  @Autowired
-  JwtUtils jwtUtils;
+    @Autowired
+    JwtUtils jwtUtils;
 
-  @Autowired
-  AuditService auditService;
-  public void saveAudit(String apiname,String data){
-    auditService.saveAudit(apiname,data);
-  }
-  @PostMapping("/signin")
-  public ResponseEntity<?> authenticateUser( @RequestBody LoginRequest loginRequest) {
-    saveAudit("/api/auth/signin","login a user with userName  "+loginRequest.getUsername());
-    if (StringUtils.isBlank(loginRequest.getUsername())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Username should not be blank"));
+    @Autowired
+    AuditService auditService;
+
+    public void saveAudit(String apiname, String data) {
+        auditService.saveAudit(apiname, data);
     }
 
-    // Check if the password is blank
-    if (StringUtils.isBlank(loginRequest.getPassword())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Password should not be blank"));
-    }
-    Authentication authentication = authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = jwtUtils.generateJwtToken(authentication);
-
-    UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-    List<String> roles = userDetails.getAuthorities().stream()
-            .map(item -> item.getAuthority())
-            .collect(Collectors.toList());
-    saveAudit("/api/auth/signin","logged in  success "+userDetails.getUsername());
-    return ResponseEntity.ok(new JwtResponse(jwt,
-            userDetails.getId(),
-            userDetails.getUsername(),
-            userDetails.getEmail(),
-            roles));
-  }
-
-  @PostMapping("/signup")
-  public ResponseEntity<?> registerUser( @RequestBody SignupRequest signUpRequest) {
-    saveAudit("/api/auth/signup","register a user with user name "+ signUpRequest.getUsername());
-
-    if (StringUtils.isBlank(signUpRequest.getUsername())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Username should not be blank"));
-    }
-
-    // Check if the email is blank
-    if (StringUtils.isBlank(signUpRequest.getEmail())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Email should not be blank"));
-    }
-
-    // Check if the role is blank or empty
-    if (signUpRequest.getRole() == null || signUpRequest.getRole().isEmpty()) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Role should not be blank or empty"));
-    }
-
-    // Check if the password is blank
-    if (StringUtils.isBlank(signUpRequest.getPassword())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Password should not be blank"));
-    }
-
-    if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Username is already taken!"));
-    }
-
-    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-      return ResponseEntity
-              .badRequest()
-              .body(new MessageResponse("Error: Email is already in use!"));
-    }
-
-    // Create new user's account
-    User user = new User(signUpRequest.getUsername(),
-            signUpRequest.getEmail(),
-            encoder.encode(signUpRequest.getPassword()));
-
-    Set<String> strRoles = signUpRequest.getRole();
-    Set<Role> roles = new HashSet<>();
-
-    if (strRoles == null) {
-      Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-              .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-      roles.add(userRole);
-    } else {
-      strRoles.forEach(role -> {
-        switch (role) {
-          case "ROLE_ADMIN":
-            Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(adminRole);
-
-            break;
-          case "ROLE_MODERATOR":
-            Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(modRole);
-
-            break;
-          default:
-            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-            roles.add(userRole);
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        saveAudit("/api/auth/signin", "login a user with userName  " + loginRequest.getUsername());
+        if (StringUtils.isBlank(loginRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username should not be blank"));
         }
-      });
+
+        // Check if the password is blank
+        if (StringUtils.isBlank(loginRequest.getPassword())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Password should not be blank"));
+        }
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority()).collect(Collectors.toList());
+        saveAudit("/api/auth/signin", "logged in  success " + userDetails.getUsername());
+
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
     }
 
-    user.setRoles(roles);
-    userRepository.save(user);
-    saveAudit("/api/auth/signin","account created success "+user.getUsername());
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody SignupRequest signUpRequest) {
+        saveAudit("/api/auth/signup", "register a user with user name " + signUpRequest.getUsername());
 
-    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
-  }
+        if (StringUtils.isBlank(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username should not be blank"));
+        }
+
+        // Check if the email is blank
+        if (StringUtils.isBlank(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email should not be blank"));
+        }
+
+        // Check if the role is blank or empty
+        if (signUpRequest.getRole() == null || signUpRequest.getRole().isEmpty()) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Role should not be blank or empty"));
+        }
+
+        if (StringUtils.isBlank(signUpRequest.getPassword())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Password should not be blank"));
+        }
+
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
+        }
+        User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(), encoder.encode(signUpRequest.getPassword()));
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "ROLE_ADMIN":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "ROLE_MODERATOR":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER).orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+        saveAudit("/api/auth/signin", "account created success " + user.getUsername());
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
 }
